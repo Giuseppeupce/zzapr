56979b728b0aaaceb1aafc679104789e7e7d808d
Giuseppe Ortolano <giuseppeortolano3@gmail.com>
2023-02-24 11:22:04 +0100

    init commit

--- zzapr/Student.java
+++ zzapr/Student.java
@@ -2,6 +2,6 @@ package zzapr;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Giuseppe Ortolano, University of Palermo, innovation engineering for digital business");
     }
 }
