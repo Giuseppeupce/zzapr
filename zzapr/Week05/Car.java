baa4ff5d6f3fa0529daa97de293540fd9f114357
Giuseppe Ortolano <giuseppeortolano3@gmail.com>
2023-03-24 11:20:58 +0100

    week5

---
+++ zzapr/Week05/Car.java
@@ -0,0 +1,42 @@
+package zzapr.Week05;
+
+public class Car {
+
+    private String manufacture;
+    private String type;
+    private int pricePaid;
+    private int odometer;
+
+
+    public Car(String manufacture, String type, int pricePaid, int odometer) {
+        this.manufacture = manufacture;
+        this.type = type;
+        this.pricePaid = pricePaid;
+        this.odometer = odometer;
+    }
+
+    public int getPricePaid() {
+        return pricePaid;
+    }
+
+    @Override
+        public String toString() {
+            String text = "";
+
+            text += this.manufacture + " " + this.type + ": " + this.pricePaid + "EUR, "+ "milwage: " +
+                    this.odometer + " km";
+
+
+
+
+            return text;
+        }
+
+    }
+
+
+
+
+
+
+
